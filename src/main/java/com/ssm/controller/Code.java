4978225a1f744613f1767536a8ff2bd957255731
Peter0411 <262901609@qq.com>
2023-12-17 12:21:18 +0800

    Add login and register

--- src/main/java/com/ssm/controller/Code.java
+++ src/main/java/com/ssm/controller/Code.java
@@ -6,11 +6,16 @@ public class Code {
     public static final Integer DELETE_OK=20031;
     public static final Integer GET_OK=20041;
 
+    public static final Integer LOGIN_OK=20051;
+    public static final Integer REGISTER_OK=20061;
+
     public static final Integer SAVE_ERR=20010;
     public static final Integer UPDATE_ERR=20020;
     public static final Integer DELETE_ERR=20030;
     public static final Integer GET_ERR=20040;
 
+    public static final Integer LOGIN_ERR=20050;
+    public static final Integer REGISTER_ERR=20060;
 
     public static final Integer SYSTEM_ERR=50001;
     public static final Integer SYSTEM_OUT_ERR=50002;
